e656ac0eee9fe8b255a3d385654414e821d2d4ab
Ondřej Hošek <ondra.hosek@gmail.com>
2022-01-19 07:10:13 +0100

    geojson output: also output node IDs as attribute

--- src/main.rs
+++ src/main.rs
@@ -772,17 +772,20 @@ macro_rules! eprintlntime {
 
 fn path_to_geojson<'a, I: Iterator<Item = &'a PathSegment<'a>>>(segments: I) -> serde_json::Value {
     let mut line_string_coords = Vec::new();
+    let mut node_ids = Vec::new();
     for segment in segments {
         if line_string_coords.len() == 0 {
             line_string_coords.push(serde_json::json!([
                 segment.start_node.lon(),
                 segment.start_node.lat(),
             ]));
+            node_ids.push(segment.start_node.id.0);
         }
         line_string_coords.push(serde_json::json!([
             segment.end_node.lon(),
             segment.end_node.lat(),
         ]));
+        node_ids.push(segment.end_node.id.0);
     }
 
     serde_json::json!({
@@ -790,6 +793,9 @@ fn path_to_geojson<'a, I: Iterator<Item = &'a PathSegment<'a>>>(segments: I) -> 
         "geometry": {
             "type": "LineString",
             "coordinates": line_string_coords,
+        },
+        "attributes": {
+            "node_ids": node_ids,
         },
     })
 }
